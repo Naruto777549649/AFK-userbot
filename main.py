3b395235994ebd9e4ab2de7b781bf9f0941d941d
Naruto777549649 <sikrar026@gmail.com>
2025-04-04 09:23:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, SESSION_STRING
 
+app = Client("userbot", api_id=API_ID, api_hash=API_HASH, session_string=SESSION_STRING)
+
+# Import Features
+import afk
+import antitag
+import commands
+
+print("🚀 Userbot is Running!")
+app.run()
